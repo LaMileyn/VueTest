a99af42dfc4710daffa0d89c32b7d3519cc56745
Volg <lost-is-died@mail.ru>
2022-04-30 17:06:29 +0500

    Start of Project ( install fonts, create scss variables )

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="mainContainer"></div>
 </template>
 
 <script>
@@ -11,13 +11,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+#app{
+  min-height: 100vh;
 }
+
 </style>
